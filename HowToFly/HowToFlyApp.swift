f61da6401631c2a0566531d5aa3d2c79731b0423
Coooder <1637120528@qq.com>
2025-02-11 19:59:27 +0800

    Initial Commit

---
+++ HowToFly/HowToFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToFlyApp.swift
+//  HowToFly
+//
+//  Created by Coooder on 2025/2/11.
+//
+
+import SwiftUI
+
+@main
+struct HowToFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HowToFlyTests/HowToFlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  HowToFlyTests.swift
+//  HowToFlyTests
+//
+//  Created by Coooder on 2025/2/11.
+//
+
+import Testing
+
+struct HowToFlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
